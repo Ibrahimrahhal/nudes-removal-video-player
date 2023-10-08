8bc8f4d97803d4d83d4c33a3242e6d354c73f000
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2023-10-08 20:45:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 import cv2
 from PIL import Image, ImageTk
+import pygame
+from pygame.locals import *
 from moviepy.editor import VideoFileClip
 
 class VideoPlayer:
@@ -9,24 +11,30 @@ class VideoPlayer:
         self.root = root
         self.root.title("Video Player")
 
-        # Create a frame for the video display
         self.frame = tk.Frame(self.root)
         self.frame.pack()
 
-        # Create a label for displaying video frames
         self.video_label = ttk.Label(self.frame)
         self.video_label.pack()
 
-        self.video_clip = VideoFileClip('video.mp4'
+        self.video_clip = VideoFileClip('your_video.mp4') 
         self.audio_clip = self.video_clip.audio
+        self.audio_initialized = False
 
         self.update()
 
     def update(self):
-        ret, frame = self.video_clip.read_frame()
-        if ret:
+        frame = self.video_clip.get_frame(self.video_clip.get_time())
+        if frame is not None:
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             self.display_frame(frame)
+
+            if not self.audio_initialized:
+                pygame.mixer.init()
+                pygame.mixer.music.load(self.audio_clip.fps, self.audio_clip.reader.nchannels)
+                pygame.mixer.music.set_volume(1.0)
+                pygame.mixer.music.play()
+                self.audio_initialized = True
         else:
             self.video_clip.close()
 
