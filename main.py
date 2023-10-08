cae36f1167bf6a609969ae98c63bba80f59d844e
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2023-10-08 20:42:12 +0300

    audio support

--- main.py
+++ main.py
@@ -2,30 +2,33 @@ import tkinter as tk
 from tkinter import ttk
 import cv2
 from PIL import Image, ImageTk
+from moviepy.editor import VideoFileClip
 
 class VideoPlayer:
     def __init__(self, root):
         self.root = root
         self.root.title("Video Player")
 
+        # Create a frame for the video display
         self.frame = tk.Frame(self.root)
         self.frame.pack()
 
+        # Create a label for displaying video frames
         self.video_label = ttk.Label(self.frame)
         self.video_label.pack()
 
-        self.video_capture = cv2.VideoCapture('video.mp4') 
+        self.video_clip = VideoFileClip('video.mp4'
+        self.audio_clip = self.video_clip.audio
 
         self.update()
 
     def update(self):
-        ret, frame = self.video_capture.read()
+        ret, frame = self.video_clip.read_frame()
         if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             self.display_frame(frame)
         else:
-            # If the video ends, you can add logic to handle this event
-            self.video_capture.release()
+            self.video_clip.close()
 
         self.root.after(10, self.update)
 
